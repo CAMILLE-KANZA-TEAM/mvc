77e621ed88e76b8ebe3713673195e87de80b6e1d
ckanza <17140588+ckanzafluks@users.noreply.github.com>
2021-12-29 19:26:20 +0100

    add mvc archi

---
+++ Controllers/IndexController.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * 
+ * 
+ */
+class IndexController extends BaseController {
+
+	// Page d'accueil
+	public function index() {
+		// on choisi la template à appeler
+		$template = $this->twig->load('index/index.html');
+
+		// Puis on affiche la page avec la méthode render
+		echo $template->render([]);
+	}
+
+}
